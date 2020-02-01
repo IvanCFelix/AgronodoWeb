831732919ebe609067c7f057b237e7ff7f89699f
luiscastro8p <luisarmandocastro12@gmail.com>
2020-01-31 17:33:35 -0700

    [

--- src/app/routes/agricola-agronodo/agricola-agronodo.component.ts
+++ src/app/routes/agricola-agronodo/agricola-agronodo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: "app-agricola-agronodo",
@@ -6,11 +7,12 @@ import { Component, OnInit, ViewChild } from "@angular/core";
   styleUrls: ["./agricola-agronodo.component.scss"]
 })
 export class AgricolaAgronodoComponent implements OnInit {
-  rowsExp = [];
+  listagricola = [];
   timeout: any;
   expanded: any = {};
   selected = [];
-  
+  temp = [];
+
   constructor() {}
 
   onPage(event) {
@@ -18,7 +20,8 @@ export class AgricolaAgronodoComponent implements OnInit {
     this.timeout = setTimeout(() => {
     }, 100);
   }
- 
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+
   @ViewChild("myTable") tableExp: any;
 
 
@@ -33,4 +36,16 @@ export class AgricolaAgronodoComponent implements OnInit {
 
   onActivate(event) {
   }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+        return d.names.toLowerCase().indexOf(val) !== -1  || d.lastnames.toLowerCase().indexOf(val) !== -1  || d.user.username.toLowerCase().indexOf(val) !== -1 || d.user.email.toLowerCase().indexOf(val) !== -1 || d.phone.toLowerCase().indexOf(val) !== -1 || !val ;
+    });
+    // update the rows
+    this.listagricola = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+}
 }
