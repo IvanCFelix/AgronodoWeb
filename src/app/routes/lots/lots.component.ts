fa2fe4d1b167c5f93940c7ee3d98cf59ffd347f6
luiscastro8p <luisarmandocastro12@gmail.com>
2020-02-19 16:03:01 -0700

    [UPDATE]Se agreg en lots

--- src/app/routes/lots/lots.component.ts
+++ src/app/routes/lots/lots.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import Swal from "sweetalert2";
 @Component({
   selector: 'app-lots',
   templateUrl: './lots.component.html',
   styleUrls: ['./lots.component.scss']
 })
 export class LotsComponent implements OnInit {
-
+  @ViewChild("table") tableExp: any;
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+  filterPost = "";
+  editing = {};
+  listlots = [];
+  temp = [];
+  timeout: any;
+  expanded: any = {};
+  selected = [];
   constructor() { }
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+        return d.names.toLowerCase().indexOf(val) !== -1  || d.lastnames.toLowerCase().indexOf(val) !== -1  || d.user.username.toLowerCase().indexOf(val) !== -1 || d.user.email.toLowerCase().indexOf(val) !== -1 || d.phone.toLowerCase().indexOf(val) !== -1 || !val ;
+    });
+    // update the rows
+    this.listlots = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+}
 
   ngOnInit() {
+    // this.AdminagronodoService.listadmin().subscribe(resp => {
+    //   this.listlots = resp;
+    //   this.temp = resp;
+    //   console.log(resp)
+    // })
   }
 
+  delete(value){
+    // console.log(value)
+    // Swal.fire({
+    //   title: 'Seguro que quieres eliminar a',
+    //   text: value.names,
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Si, eliminar!',
+    //   cancelButtonText: 'No, cancelar!',
+    // }).then((result) => {
+    //   if (result.value) {
+    //     console.log(result.value)
+    //     this.AdminagronodoService.delete(value.user.username)
+    //     .subscribe( resp => {
+          
+    //       this.AdminagronodoService.listadmin().subscribe(resp => {
+    //         this.listAdmin = resp;
+    //         this.temp = resp;
+    //         console.log(resp)
+    //       })
+
+    //     })
+    //   }
+    // })
+  }
+  
+  onPage(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+    }, 100);
+  }
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
 }
+ 
+  onSelect({ selected }) {
+
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+
+  onActivate(event) {
+  }
+}
+
+
+
+
