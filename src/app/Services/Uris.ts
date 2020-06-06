e6074f43bfac63f9318ad9acd0fb4b596f8eb529
luiscastro8p <luisarmandocastro12@gmail.com>
2020-06-05 21:48:20 -0600

    .\src\app\Services\Uris.ts

--- src/app/Services/Uris.ts
+++ src/app/Services/Uris.ts
@@ -1,5 +1,5 @@
 export class Uris {
-         public static API_ENDPOINT = "http://159.89.49.19/api/v1/";
+         public static API_ENDPOINT = "http://159.89.49.19:8000/api/v1/";
          //public static API_ENDPOINT = "http://192.168.50.103:8000/api/v1/";
 
          public static PASSWORD_RESET = `${Uris.API_ENDPOINT}password/reset/confirm/`;
